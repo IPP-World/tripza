549a97041646a52b8d50e3883a5f5be679122cbc
Kripesh Pathak <kripeshpathak385@gmail.com>
2023-02-11 11:11:31 +0545

    push

--- src/pages/Agencies.jsx
+++ src/pages/Agencies.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+//import Navbar from '../components/Navbar';
+import Profile from './Profile';
 
 export default function Agencies(){
     return<div>
-        Agencies
-    </div>
+        <Profile/> </div>
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,40 @@
-export default function Hotels({}){
-    return <div>Profile</div>
+import { CgProfile } from "react-icons/Cg";
+export default function Profile({}) {
+  return (
+    <div className="profile-container">
+      <div className="profile-leftpart">
+        <div className="profile-pic">
+          <CgProfile /><br/>
+          <label className="name">John Doe</label>
+          <br />
+          <label className="role">General user</label>
+        </div>
+        <button className="edit-btn">Edit</button>
+        <div className="level">
+          <div className="level-bar"></div>
+          <label>Level 1</label>
+          <br />
+          <label>69/100</label>
+        </div>
+      </div>
+      <div className="profile-rightpart">
+        <p>Recent Contributions</p>
+        <div className="recent-container">
+          <ul>
+            <li>
+              <p>Owl Bamboo House</p>
+              <br />
+              <p>Sidemen,Bali,Indonesia</p>
+            </li>
+            <li>
+              <p>Owl Bamboo House</p>
+              <br />
+              <p>Sidemen,Bali,Indonesia</p>
+            </li>
+          </ul>
+        </div>
+        <button className="more-btn">More</button>
+      </div>
+    </div>
+  );
 }
