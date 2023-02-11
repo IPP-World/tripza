b19afcf598b9c93c902834ec048a2b91faaa4bf6
Kripesh Pathak <kripeshpathak385@gmail.com>
2023-02-11 11:33:54 +0545

    pull the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import khaltiLogo from "../assets/khaltiLogo.png";
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import "./Navbar.css";
 function Navbar() {
   return (
-    <div className="nav--container">
-      <Link to="/"><img className="nav--logo" src={khaltiLogo} alt="Logo"/></Link>
-      <input className="nav--search" type="text" placeholder="&#xF002; Search for places/destinations"/>
-      <Link className="nav--links" to="/agencies">Agencies</Link>
-      <Link className="nav--links" to="/hotels">Hotels</Link>
-      <Link className="nav--links" to="/contribute">Contribute</Link>
-      <div className="nav--signup_login">
-        <Link className="nav--links" to="/login">Login</Link> | <Link className="nav--links" to="/signup">Signup</Link>
+    <div className="nav-container">
+      <Link to="/">Tripza</Link>
+      <input  className="logo" type="text" onSubmit={()=>{console.log("Submitting")}} placeholder="Search for places/destinations"/>
+      <Link to="/agencies">Agencies</Link>
+      <Link to="/hotels">Hotels</Link>
+      <Link to="/contribute">Contribute</Link>
+      <div>
+          ?{<Link to="/login">Login</Link> / <Link to="/signup">Signup</Link>}:
       </div>
     </div>
   );
