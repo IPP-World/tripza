3317971afad5726a43a3901adf16e325abc9bd36
Indira <indisharma314@gmail.com>
2023-02-13 02:22:36 +0545

    contribute css

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -2,54 +2,75 @@ import { GrAdd } from "react-icons/Gr";
 import { AiFillStar } from "react-icons/Ai";
 import { AiOutlineStar } from "react-icons/Ai";
 import React, { useState } from "react";
+import screenshot from "../assets/Screenshot (40).png"
 import "./Contribute.css";
 
-
 export default function Contribute() {
-   const [showModal, setShowModal] =useState(false);
-   const Popup =() =>{
-    return(<>
-    <div className="reward-wrapper"> </div>
-      <div className="reward-container">
-    <button className="reward--close-btnn" onClick={()=>setShowModal(false)}>close</button>
-        <p>Congratulations<br/>You contributed a place</p>
-        <div className="reward-points">
+  const [showModal, setShowModal] = useState(false);
+  const Popup = () => {
+    return (
+      <>
+        <div className="reward-wrapper"> </div>
+        <div className="reward-container">
+          <button
+            className="reward--close-btnn"
+            onClick={() => setShowModal(false)}
+          >
+            close
+          </button>
+          <p>
+            Congratulations
+            <br />
+            You contributed a place
+          </p>
+          <div className="reward-points"></div>
         </div>
-        </div>
-    </>
+      </>
     );
-  }
-  const submitHandler = (e)=>{
-      e.preventDefault();
-      setShowModal(true)
-  }
- 
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setShowModal(true);
+  };
+
   return (
     <div className="contribute-container">
       <div className="left-part">
-        <p className="contribute-text">Contribute a Place</p>
+        <div className="contribute-text">Contribute a Place</div>
         <div className="places-container">
           <ul>
             <li>
-              <img src="/vite.svg"></img>
+              <img src={screenshot}></img>
             </li>
             <li>
-              <span>
-                <GrAdd />
+              <span className="add-box">
+                <GrAdd className="add"/>
               </span>
             </li>
           </ul>
         </div>
-        <div className="maps-container"></div>
+        <div className="maps-container">
+        <img className="map" src={screenshot}></img>
+        </div>
       </div>
       <div className="right-part">
         <div className="place-info">
           <form method="#">
-            <input className="place-name" type="text" placeholder="Name of the place" />
-            <input className="place-name" type="text" placeholder="Description of the place" />
-            <p>What this place offers</p>
-            <div className="place-offers"></div>
-            <p>Your review of the place</p>
+            <input
+              className="place-name1"
+              type="text"
+              placeholder="Name of the place"
+            />
+            <input
+              className="place-name2"
+              type="textbox"
+              placeholder="Description of the place"
+            />
+            <div className="place-offers-text">What this place offers</div>
+            <div className="place-offers">
+              
+            </div>
+            <div className="review-text">Your review of the place</div>
             <div className="review">
               <span>
                 <AiFillStar />
@@ -66,21 +87,23 @@ export default function Contribute() {
               <span>
                 <AiOutlineStar />
               </span>
-              </div>
+            </div>
+            <div className="review-box">
               <input type="text" placeholder="Review Here" />
-              <label>
-                <input type="checkbox" />
-                The information I submitted here is legit.
-              </label>
-              <br />
-           
-
-            <button onClick={submitHandler}>Submit </button>
-            {showModal && <Popup/>}
+            </div>
+            <div className="checkbox">
+            <label required>
+              <input type="checkbox" />
+              The information I submitted here is legit.
+            </label></div>
+            <br />
+          <button className="submit" onClick={submitHandler}>
+              Submit
+            </button>
+            {showModal && <Popup />}
           </form>
         </div>
       </div>
     </div>
   );
 }
-
