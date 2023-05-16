1b33a3400e5634da86b4595e5ef607301600229c
Kripesh Pathak <kripeshpathak385@gmail.com>
2023-05-16 14:46:37 +0545

    satellite map+ device location

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,8 +1,18 @@
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap,  LayersControl} from 'react-leaflet';
 import React, { useState, useEffect } from "react";
 
+const Recenter = ({ lat, lng }) => {
+  const map = useMap();
+  
+  useEffect(() => {
+    map.setView([lat, lng]);
+  }, [map, lat, lng]);
+  
+  return null;
+};
+
 const MapSection = ({ onLocationSelect }) => {
-  const [mapCenter, setMapCenter] = useState([25.255673, 83.978599]);
+  const [mapCenter, setMapCenter] = useState([30.255673, 83.978599]);
   const [selectedLocation, setSelectedLocation] = useState(null);
 
   useEffect(() => {
@@ -25,7 +35,7 @@ const MapSection = ({ onLocationSelect }) => {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject, {
         enableHighAccuracy: true,
-        timeout: 5000,
+        timeout: 10000,
         maximumAge: 0
       });
     });
@@ -50,7 +60,6 @@ const MapSection = ({ onLocationSelect }) => {
     return null;
   };
   
-
   return (
     <>
       <MapContainer
@@ -59,12 +68,25 @@ const MapSection = ({ onLocationSelect }) => {
         scrollWheelZoom={true}
         style={{ width: "100%", height: "100%", maxWidth: "600px", maxHeight: "500px" }}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Street View">
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite View">
+            <TileLayer
+              url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+              maxZoom={20}
+              subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
         <MapClickHandler onMapClick={handleMapClick} />
         {selectedLocation && <Marker position={selectedLocation} />}
+        {mapCenter && <Recenter lat={mapCenter[0]} lng={mapCenter[1]} />}
       </MapContainer>
     </>
   );
 };
 
 export default MapSection;
+ 
