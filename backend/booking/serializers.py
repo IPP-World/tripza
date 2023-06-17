8fea0f16d543e7209873be2bf358827032f52316
kamlesh-kd <kranabhat.krb@gmail.com>
2023-06-18 02:48:23 +0545

    Added booking confirmation

--- backend/booking/serializers.py
+++ backend/booking/serializers.py
@@ -4,7 +4,7 @@ from .models import Booking
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id', 'user', 'hotel','h_name', 'check_in_date', 'check_out_date', 'guest_no', 'room_no']
+        fields = ['id', 'user', 'hotel','h_name', 'check_in_date', 'check_out_date', 'guest_no', 'room_no', 'status']
 
     def create(self, validated_data):
         booking = Booking.objects.create(**validated_data)
